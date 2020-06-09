fb942a527e651d9db9fe97ead06090e25b0cee07
Bazookan <43674144+Bazookan@users.noreply.github.com>
2020-06-09 03:38:28 +0200

    Use fxmanifest instead

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "bodacious"
+
+game "gta5"
+
+description "esx_sopletare"
+
+version "1.0.1"
+
+client_script "client/main.lua"
+
+server_script "server/main.lua"
+
+shared_script "config.lua"
