e5a506375fb001b5f50d6c81de717b809d80dbee
Bazookan <43674144+Bazookan@users.noreply.github.com>
2020-06-09 03:38:55 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# baz_dumpstersearch
+
+[NOTES]
+
+* You can change items, translation and more in the config.
+
+* You can change the loadingbar to another one if you want (client.lua, row 70).
+
+[REQUIREMENTS]
+  
+* https://github.com/esx-org/es_extended
+
+* https://github.com/t0sic/t0sic_loadingbar (Read notes)
+
+[INSTALLATION]
+
+1) Download: https://github.com/Bazookan/
+
+2) Add this in your server.cfg :
+``ensure baz_dumpstersearch``
+
+3) All done!
---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {
+	EnableWeapons = false, -- If you want the players to be able to find weapons (false by default).
+
+	Dumpsters = { -- Props of the dumpsters wich is getting defined in the client.
+		"prop_dumpster_01a",
+		"prop_dumpster_02a",
+		"prop_dumpster_02b"
+	},
+
+	Items = { -- Add whatever items you want here.
+        "fishing_lure",
+        "bandage",
+        "weed_pooch",
+        "bread",
+        "lotteryticket"
+	},
+	
+	Weapons = { -- Add whatever weapons you want here (don't care about this if EnableWeapons = false).
+        "WEAPON_KNIFE",
+        "WEAPON_PISTOL"
+    }
+}
+
+Strings = { -- Translation
+	["Search"] = "Press [~g~E~s~] to search",
+	["Searched"] = "You've already searched this dumpster!",
+	["Found"] = "You found ",
+	["Searching"] = "Searching...",
+	["Nothing"] = "You found nothing..."
+}
