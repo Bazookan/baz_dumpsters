182b0a6c4f9f1d387ba439000b195d73c04df8ae
Bazookan <43674144+Bazookan@users.noreply.github.com>
2020-06-18 17:35:04 +0200

    Added check if player actually can carry the item.

--- server/main.lua
+++ server/main.lua
@@ -13,16 +13,26 @@ ESX.RegisterServerCallback("baz_dumpstersearch:getItem", function(source, cb)
             local randomItem = Config["Items"][math.random(#Config["Items"])]
             local quantity = math.random(#Config["Items"])
             local itemLabel = ESX.GetItemLabel(randomItem)
-            player.addInventoryItem(randomItem, quantity)
-            cb(true, itemLabel, quantity)
+
+            if player.canCarryItem(randomItem, quantity) then
+                player.addInventoryItem(randomItem, quantity)
+                cb(true, itemLabel, quantity)
+            else
+                cb(false)
+            end
         else
             if Config["EnableWeapons"] then -- disabled by default, enable in the config.
                 if luck == 2 then
                     local randomWeapon = Config["Weapons"][math.random(#Config["Weapons"])]
                     local ammunition = math.random(#Config["Weapons"])
                     local weaponLabel = ESX.GetWeaponLabel(randomWeapon)
-                    player.addWeapon(randomWeapon, ammunition)
-                    cb(true, weaponLabel, 1)
+
+                    if player.hasWeapon(randomWeapon) then
+                        cb(false)
+                    else
+                        player.addWeapon(randomWeapon, ammunition)
+                        cb(true, weaponLabel, 1)
+                    end
                 else
                     cb(false)
                 end
