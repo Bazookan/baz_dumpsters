666ad5b3f569f5d4bddcbfa07bb269e1cd4c7fed
Bazookan <43674144+Bazookan@users.noreply.github.com>
2020-06-19 12:24:29 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -4,18 +4,18 @@ TriggerEvent("esx:getSharedObject", function(response)
     ESX = response
 end)
 
-ESX.RegisterServerCallback("baz_dumpstersearch:getItem", function(source, cb)
-    local player = ESX.GetPlayerFromId(source)
-    local luck = math.random(1, 6)
+ESX["RegisterServerCallback"]("baz_dumpsters:getItem", function(source, cb)
+    local player = ESX["GetPlayerFromId"](source)
+    local luck = math["random"](1, 6)
 
     if player then
         if luck == 4 then
-            local randomItem = Config["Items"][math.random(#Config["Items"])]
-            local quantity = math.random(#Config["Items"])
-            local itemLabel = ESX.GetItemLabel(randomItem)
+            local randomItem = Config["Items"][math["random"](#Config["Items"])]
+            local quantity = math["random"](#Config["Items"])
+            local itemLabel = ESX["GetItemLabel"](randomItem)
 
-            if player.canCarryItem(randomItem, quantity) then
-                player.addInventoryItem(randomItem, quantity)
+            if player["canCarryItem"](randomItem, quantity) then
+                player["addInventoryItem"](randomItem, quantity)
                 cb(true, itemLabel, quantity)
             else
                 cb(false)
@@ -23,14 +23,14 @@ ESX.RegisterServerCallback("baz_dumpstersearch:getItem", function(source, cb)
         else
             if Config["EnableWeapons"] then -- disabled by default, enable in the config.
                 if luck == 2 then
-                    local randomWeapon = Config["Weapons"][math.random(#Config["Weapons"])]
-                    local ammunition = math.random(#Config["Weapons"])
-                    local weaponLabel = ESX.GetWeaponLabel(randomWeapon)
+                    local randomWeapon = Config["Weapons"][math["random"](#Config["Weapons"])]
+                    local ammunition = math["random"](#Config["Weapons"])
+                    local weaponLabel = ESX["GetWeaponLabel"](randomWeapon)
 
-                    if player.hasWeapon(randomWeapon) then
+                    if player["hasWeapon"](randomWeapon) then
                         cb(false)
                     else
-                        player.addWeapon(randomWeapon, ammunition)
+                        player["addWeapon"](randomWeapon, ammunition)
                         cb(true, weaponLabel, 1)
                     end
                 else
